d6b19eff6ecacab0639549afc63c540e5c81eb1e
ori <ori@vava33.com>
2015-11-18 18:53:00 +0100

    "add user guide, update about/help dialogs and icons"

--- src/vava33/d2dplot/About_dialog.java
+++ src/vava33/d2dplot/About_dialog.java
@@ -1,11 +1,9 @@
 package vava33.d2dplot;
 
 import java.awt.BorderLayout;
+import java.awt.Desktop;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Image;
-import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,7 +16,18 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
-import java.awt.Font;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.JEditorPane;
+import javax.swing.JScrollPane;
+
+import com.vava33.jutils.FileUtils;
 
 public class About_dialog extends JDialog {
 
@@ -27,19 +36,22 @@ public class About_dialog extends JDialog {
     private JButton btnUsersGuide;
     private final JPanel contentPanel = new JPanel();
     private JLabel lblTalplogo;
+    private JEditorPane textPane;
+    private JScrollPane scrollPane;
+    private JLabel lblLogoalba;
 
     /**
      * Launch the application.
      */
-    public static void main(String[] args) {
-        try {
-            About_dialog dialog = new About_dialog();
-            dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            dialog.setVisible(true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        try {
+//            About_dialog dialog = new About_dialog();
+//            dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+//            dialog.setVisible(true);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     /**
      * Create the dialog.
@@ -47,53 +59,25 @@ public class About_dialog extends JDialog {
     public About_dialog() {
         setIconImage(Toolkit.getDefaultToolkit().getImage(About_dialog.class.getResource("/img/Icona.png")));
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        setTitle("About Plot2D-XRD");
-        // setBounds(100, 100, 660, 730);
+        setTitle("About d2Dplot");
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        int width = 660;
-        int height = 730;
+        int width = 680;
+        int height = 780;
         int x = (screen.width - width) / 2;
         int y = (screen.height - height) / 2;
-        setBounds(x, y, 570, 680);
+        setBounds(x, y, 680, 780);
         getContentPane().setLayout(new BorderLayout());
         this.contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(this.contentPanel, BorderLayout.CENTER);
-        GridBagLayout gbl_contentPanel = new GridBagLayout();
-        gbl_contentPanel.columnWidths = new int[] { 0, 0 };
-        gbl_contentPanel.rowHeights = new int[] { 0, 0, 0 };
-        gbl_contentPanel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-        gbl_contentPanel.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
-        contentPanel.setLayout(gbl_contentPanel);
+        contentPanel.setLayout(new MigLayout("", "[grow]", "[64px][grow][]"));
         {
             lblTalplogo = new JLabel("** LOGO **");
-            GridBagConstraints gbc_lblTalplogo = new GridBagConstraints();
-            gbc_lblTalplogo.insets = new Insets(2, 5, 5, 5);
-            gbc_lblTalplogo.gridx = 0;
-            gbc_lblTalplogo.gridy = 0;
-            contentPanel.add(lblTalplogo, gbc_lblTalplogo);
-        }
-        {
-            JLabel lblAbout = new JLabel(
-//                    "<html> \r\n<div style=\"text-align:center\"> \r\n<b><font size=+0>Oriol Vallcorba</font></b>\r\n<br>\r\nExperiments Division - MSPD Beamline (BL04)<br>\r\nALBA Synchrotron Light Source - CELLS (www.cells.es)<br>\r\n[Previous affiliation: Institut de Ci\u00E8ncia de Materials de Barcelona (ICMAB-CSIC)]<br>\r\n<br>\r\n<b><font size=+0>Jordi Rius</font></b>\r\n<br>\r\nInstitut de Ci\u00E8ncia de Materials de Barcelona (ICMAB)<br>\r\nConsejo Superior de Investigaciones Cient\u00EDficas (CSIC)<br>\r\n<br>\r\nFor comments/errors/suggestions, please contact to: <strong>ovallcorba@cells.es</strong><br>\r\n</div>\r\n<br>\r\n<div style=\"text-align:justify\"> \r\n<b><font size=+0>Conditions of use</font></b><br>\r\nThis software can be used free of charge for non-commercial academic purposes only. For any other purpose, please contact directly with the author. Further distribution of this software is not allowed.<br>\r\nCitation of the reference <u>XXXX</u> would be greatly appreciated when publishing works done with it.\r\n<br><br>\r\n<b><font size=+0>Disclaimer</font></b><br>\r\nThis software is distributed WITHOUT ANY WARRANTY. The authors (or their institutions) have no liabilities in respect of errors in the software, in the documentation and in any consequence of erroneous results or damages arising out of the use or inability to use this software. Use it at your own risk.\r\n<br><font size=-2>&nbsp;<br></font>\r\n<font size=-1>D2Dplot is programmed with Java&trade;</font>\r\n<br><br>\r\n<b><font size=+0>Acknowledgements</font></b><br>\r\nThanks are due the Spanish \u2018Ministerio de Ciencia e Innovaci\u00F3n\u2019 and to the \u2018Generalitat the Catalunya\u2019 for continued financial support.\r\n<br><br>\r\n<font size=-1>\r\n&copy; O.Vallcorba 2014 <br>\r\nICMAB-CSIC & ALBA-CELLS.\r\n</font>\r\n<br>\r\n</div>\r\n</html> ");
-                    "<html> <div style=\"text-align:center\"> Author: <b><font size=+0>Oriol Vallcorba</font></b><br>Experiments Division - MSPD Beamline (BL04)<br>ALBA Synchrotron Light Source - CELLS (www.cells.es)<br><br><font size=-1>Collaborator: <b> Jordi Rius</b><br>Institut de Ci\u00E8ncia de Materials de Barcelona (ICMAB)<br>Consejo Superior de Investigaciones Cient\u00EDficas (CSIC)</font><br><br>For comments/errors/suggestions, please contact to: <strong>ovallcorba@cells.es</strong><br></div><br><div style=\"text-align:justify\"> <b><font size=+0>Conditions of use</font></b><br>This software can be used free of charge for non-commercial academic purposes only. For any other purpose, please contact directly with the author. Further distribution of this software is not allowed.<br>Citation of the author/program/affiliation would be greatly appreciated when publishing works done with it.<br><br><b><font size=+0>Disclaimer</font></b><br>This software is distributed WITHOUT ANY WARRANTY. The authors (or their institutions) have no liabilities in respect of errors in the software, in the documentation and in any consequence of erroneous results or damages arising out of the use or inability to use this software. Use it at your own risk.<br><font size=-2>&nbsp;<br></font><font size=-1>D2Dplot is programmed with Java&trade;</font><br><br><b><font size=+0>Acknowledgements</font></b><br>Thanks are due the Spanish \u2018Ministerio de Ciencia e Innovaci\u00F3n\u2019 and to the \u2018Generalitat the Catalunya\u2019 for continued financial support.<br><br><font size=-1>&copy; O.Vallcorba 2014 <br>ALBA-CELLS.</font><br></div></html>");
-            lblAbout.setFont(new Font("Dialog", Font.PLAIN, 12));
-            GridBagConstraints gbc_lblAbout = new GridBagConstraints();
-            gbc_lblAbout.insets = new Insets(0, 5, 0, 5);
-            gbc_lblAbout.fill = GridBagConstraints.HORIZONTAL;
-            gbc_lblAbout.gridx = 0;
-            gbc_lblAbout.gridy = 1;
-            contentPanel.add(lblAbout, gbc_lblAbout);
+            contentPanel.add(lblTalplogo, "flowx,cell 0 0,alignx center,aligny center");
         }
         {
             JPanel buttonPane = new JPanel();
             getContentPane().add(buttonPane, BorderLayout.SOUTH);
             {
-                GridBagLayout gbl_buttonPane = new GridBagLayout();
-                gbl_buttonPane.columnWidths = new int[] { 468, 103, 0 };
-                gbl_buttonPane.rowHeights = new int[] { 25, 0 };
-                gbl_buttonPane.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
-                gbl_buttonPane.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
-                buttonPane.setLayout(gbl_buttonPane);
                 {
                     this.btnUsersGuide = new JButton("User's Guide");
                     this.btnUsersGuide.addActionListener(new ActionListener() {
@@ -102,12 +86,8 @@ public class About_dialog extends JDialog {
                             do_btnUsersGuide_actionPerformed(arg0);
                         }
                     });
-                    GridBagConstraints gbc_btnUsersGuide = new GridBagConstraints();
-                    gbc_btnUsersGuide.anchor = GridBagConstraints.NORTHWEST;
-                    gbc_btnUsersGuide.insets = new Insets(5, 5, 5, 5);
-                    gbc_btnUsersGuide.gridx = 0;
-                    gbc_btnUsersGuide.gridy = 0;
-                    buttonPane.add(this.btnUsersGuide, gbc_btnUsersGuide);
+                    buttonPane.setLayout(new MigLayout("", "[][grow]", "[]"));
+                    buttonPane.add(this.btnUsersGuide, "cell 0 0,alignx left,aligny top");
                 }
             }
             JButton okButton = new JButton("Close");
@@ -118,12 +98,7 @@ public class About_dialog extends JDialog {
                 }
             });
             okButton.setActionCommand("OK");
-            GridBagConstraints gbc_okButton = new GridBagConstraints();
-            gbc_okButton.insets = new Insets(5, 5, 5, 5);
-            gbc_okButton.anchor = GridBagConstraints.NORTHEAST;
-            gbc_okButton.gridx = 1;
-            gbc_okButton.gridy = 0;
-            buttonPane.add(okButton, gbc_okButton);
+            buttonPane.add(okButton, "cell 1 0,alignx right,aligny top");
             getRootPane().setDefaultButton(okButton);
         }
 
@@ -133,28 +108,81 @@ public class About_dialog extends JDialog {
         ImageIcon logo = new ImageIcon(newimg);
         lblTalplogo.setText("");
         lblTalplogo.setIcon(logo);
+        {
+            scrollPane = new JScrollPane();
+            scrollPane.setViewportBorder(null);
+            scrollPane.setOpaque(false);
+            contentPanel.add(scrollPane, "cell 0 1,grow");
+            {
+                textPane = new JEditorPane();
+                textPane.setOpaque(false);
+                scrollPane.setViewportView(textPane);
+            }
+        }
 
         // this.setIconImage(new
         // ImageIcon(getClass().getResource("/img/icona.png")).getImage());
+        
+        //llegim el fitxer html per poblar la label:
+        
+        textPane.setContentType("text/html");
+        textPane.setEditorKit(JEditorPane.createEditorKitForContentType("text/html"));
+        textPane.setEditable(false);
+        
+        textPane.addHyperlinkListener(new HyperlinkListener() {
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                if(e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                    if(Desktop.isDesktopSupported()) {
+                        try {
+                            Desktop.getDesktop().browse(e.getURL().toURI());
+                        } catch (Exception e1) {
+                            if(D2Dplot_global.isDebug())e1.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        {
+            lblLogoalba = new JLabel("");
+            lblLogoalba.setIcon(new ImageIcon(About_dialog.class.getResource("/img/ALBALogo.png")));
+            contentPanel.add(lblLogoalba, "cell 0 2,alignx center");
+        }
+        java.net.URL aboutURL = About_dialog.class.getResource("/img/about.html");
+        if (aboutURL != null) {
+            try {
+                textPane.setPage(aboutURL);
+            } catch (IOException e) {
+                System.err.println("Attempted to read a bad URL: " + aboutURL);
+            }
+        } else {
+            System.err.println("Couldn't find file: " + aboutURL);
+        }
+        
+        scrollPane.getViewport().setOpaque(false);
+       
     }
 
     protected void do_btnUsersGuide_actionPerformed(ActionEvent arg0) {
-        // try{
-        // if(Desktop.isDesktopSupported()){ // s'obre amb el programa per
-        // defecte
-        // Desktop.getDesktop().open(new
-        // File(System.getProperty("user.dir")+"\\"+userGuideFile));
-        // }else{
-        // Process p = new
-        // ProcessBuilder("cmd","/c",System.getProperty("user.dir")+"\\"+userGuideFile).start();
-        // }
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // }
-    	JOptionPane.showMessageDialog(this,
-    		    "Sorry, no user's guide yet!",
-    		    "D2Dplot",
-    		    JOptionPane.PLAIN_MESSAGE);
+        try{
+            if(Desktop.isDesktopSupported()){ // s'obre amb el programa per defecte
+                Desktop.getDesktop().open(new File(D2Dplot_global.usersGuidePath));
+                return;
+            }else{
+                if(FileUtils.confirmDialog(FileUtils.getOS(),"win")){
+                    new ProcessBuilder("cmd","/c",D2Dplot_global.usersGuidePath).start();  
+                }else{
+                    throw new Exception(); 
+                }
+                return;
+            }
+        } catch (Exception e) {
+            if(D2Dplot_global.isDebug())e.printStackTrace();
+        }
+        JOptionPane.showMessageDialog(this,
+                "Sorry, unable to open user's guide with default pdf viewer. \n"
+                + "Please open it manually from the program folder",
+                "D2Dplot User's Guide",
+                JOptionPane.PLAIN_MESSAGE);
     }
 
     protected void do_okButton_actionPerformed(ActionEvent arg0) {
--- src/vava33/d2dplot/Help_dialog.java
+++ src/vava33/d2dplot/Help_dialog.java
@@ -2,10 +2,7 @@ package vava33.d2dplot;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Image;
-import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,7 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JSeparator;
+import net.miginfocom.swing.MigLayout;
 
 public class Help_dialog extends JDialog {
 
@@ -27,20 +24,8 @@ public class Help_dialog extends JDialog {
     private JLabel lbloriolVallcorbaJordi;
 
     /**
-     * Launch the application.
-     */
-//    public static void main(String[] args) {
-//        try {
-//            Help_dialog dialog = new Help_dialog();
-//            dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-//            dialog.setVisible(true);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    /**
      * Create the dialog.
+     * @wbp.parser.constructor
      */
     public Help_dialog(String title,String text) {
         setModal(true);
@@ -49,7 +34,6 @@ public class Help_dialog extends JDialog {
         setIconImage(Toolkit.getDefaultToolkit().getImage(Help_dialog.class.getResource("/img/Icona.png")));
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setTitle(title);
-        // setBounds(100, 100, 660, 730);
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         int width = 660;
         int height = 730;
@@ -59,21 +43,9 @@ public class Help_dialog extends JDialog {
         getContentPane().setLayout(new BorderLayout());
         this.contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(this.contentPanel, BorderLayout.CENTER);
-        GridBagLayout gbl_contentPanel = new GridBagLayout();
-        gbl_contentPanel.columnWidths = new int[] { 0, 0, 0 };
-        gbl_contentPanel.rowHeights = new int[] { 0, 0, 0, 0, 0 };
-        gbl_contentPanel.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
-        gbl_contentPanel.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
-        contentPanel.setLayout(gbl_contentPanel);
         {
             JPanel buttonPane = new JPanel();
             getContentPane().add(buttonPane, BorderLayout.SOUTH);
-            GridBagLayout gbl_buttonPane = new GridBagLayout();
-            gbl_buttonPane.columnWidths = new int[] {0, 0, 0};
-            gbl_buttonPane.rowHeights = new int[] {0, 0};
-            gbl_buttonPane.columnWeights = new double[]{0.0, 1.0};
-            gbl_buttonPane.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-            buttonPane.setLayout(gbl_buttonPane);
             JButton okButton = new JButton("Close");
             okButton.addActionListener(new ActionListener() {
                 @Override
@@ -82,12 +54,7 @@ public class Help_dialog extends JDialog {
                 }
             });
             okButton.setActionCommand("OK");
-            GridBagConstraints gbc_okButton = new GridBagConstraints();
-            gbc_okButton.insets = new Insets(0, 0, 5, 5);
-            gbc_okButton.anchor = GridBagConstraints.NORTHEAST;
-            gbc_okButton.gridx = 1;
-            gbc_okButton.gridy = 0;
-            buttonPane.add(okButton, gbc_okButton);
+            buttonPane.add(okButton);
             getRootPane().setDefaultButton(okButton);
         }
 
@@ -95,55 +62,20 @@ public class Help_dialog extends JDialog {
         Image img = Toolkit.getDefaultToolkit().getImage(Help_dialog.class.getResource("/img/Icona.png"));
         Image newimg = img.getScaledInstance(-100, 64, java.awt.Image.SCALE_SMOOTH);
         ImageIcon logo = new ImageIcon(newimg);
+        contentPanel.setLayout(new MigLayout("", "[][grow]", "[grow][]"));
         {
             JLabel lbltext = new JLabel(text);
-            GridBagConstraints gbc_lbltext = new GridBagConstraints();
-            gbc_lbltext.anchor = GridBagConstraints.NORTH;
-            gbc_lbltext.fill = GridBagConstraints.HORIZONTAL;
-            gbc_lbltext.gridwidth = 2;
-            gbc_lbltext.insets = new Insets(5, 5, 5, 5);
-            gbc_lbltext.gridx = 0;
-            gbc_lbltext.gridy = 0;
-            contentPanel.add(lbltext, gbc_lbltext);
-        }
-        {
-            JSeparator separator = new JSeparator();
-            GridBagConstraints gbc_separator = new GridBagConstraints();
-            gbc_separator.gridwidth = 2;
-            gbc_separator.fill = GridBagConstraints.HORIZONTAL;
-            gbc_separator.insets = new Insets(0, 0, 5, 0);
-            gbc_separator.gridx = 0;
-            gbc_separator.gridy = 1;
-            contentPanel.add(separator, gbc_separator);
+            contentPanel.add(lbltext, "cell 0 0 2 1,growx,aligny top");
         }
         {
             lblTalplogo = new JLabel("** LOGO **");
-            GridBagConstraints gbc_lblTalplogo = new GridBagConstraints();
-            gbc_lblTalplogo.anchor = GridBagConstraints.EAST;
-            gbc_lblTalplogo.insets = new Insets(0, 5, 5, 5);
-            gbc_lblTalplogo.gridx = 0;
-            gbc_lblTalplogo.gridy = 2;
-            contentPanel.add(lblTalplogo, gbc_lblTalplogo);
+            contentPanel.add(lblTalplogo, "cell 0 1,alignx right,aligny center");
         }
         lblTalplogo.setText("");
         lblTalplogo.setIcon(logo);
         {
             lbloriolVallcorbaJordi = new JLabel("<html>\r\n<div style=\"text-align:left\"> \r\n<b>Oriol Vallcorba</b><br>\r\nALBA Synchrotron Light Source - CELLS (www.cells.es)<br>\r\nFor comments/errors/suggestions, please contact to: <strong>ovallcorba@cells.es</strong><br>\r\n</div>\r\n</html>");
-            GridBagConstraints gbc_lbloriolVallcorbaJordi = new GridBagConstraints();
-            gbc_lbloriolVallcorbaJordi.fill = GridBagConstraints.HORIZONTAL;
-            gbc_lbloriolVallcorbaJordi.insets = new Insets(0, 0, 5, 0);
-            gbc_lbloriolVallcorbaJordi.gridx = 1;
-            gbc_lbloriolVallcorbaJordi.gridy = 2;
-            contentPanel.add(lbloriolVallcorbaJordi, gbc_lbloriolVallcorbaJordi);
-        }
-        {
-            JSeparator separator = new JSeparator();
-            GridBagConstraints gbc_separator = new GridBagConstraints();
-            gbc_separator.fill = GridBagConstraints.HORIZONTAL;
-            gbc_separator.gridwidth = 2;
-            gbc_separator.gridx = 0;
-            gbc_separator.gridy = 3;
-            contentPanel.add(separator, gbc_separator);
+            contentPanel.add(lbloriolVallcorbaJordi, "cell 1 1,growx,aligny center");
         }
 
     }
